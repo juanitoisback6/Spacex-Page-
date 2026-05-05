732457f462cf6fad86d601bd856892bcde322913
Dakitidunno <207196558+juanitoisback6@users.noreply.github.com>
2026-05-04 19:30:24 -0500

    Add Starship section with details and images

--- src/app/rockets/page.tsx
+++ src/app/rockets/page.tsx
@@ -20,6 +20,38 @@ return(
     Your browser doesn't support HTML video.  
   </p>
 </video>
+
+<img className="canvaArt" src="/Vector2.png" alt="" />
+
+<section className="starcontainer">
+          <div className="rocketPartimg">
+
+                    <h2 className="rocketTitle">STARSHIP</h2>
+                    <img src="/starship.svg" alt="painter looking at the top of the page" />
+          </div>
+          <div className="rocketInfoPart">
+                    <h2>STARSHIP</h2>
+                    <p className="description">"With the ability to lift into orbit over 54 metric tons (119,000 lb)--a mass equivalent to a 737 jetliner loaded with passengers, crew, luggage and fuel--Falcon Heavy can lift more than twice the payload of the next closest operational vehicle, the Delta IV Heavy, at one-third the cost."</p>
+
+                    <div className="rocketS">
+                              <div>
+                                <h2>STAGES</h2>
+                                <p>2</p>
+                              </div>
+                              <div>
+                                <h2>BOOSTERS</h2>
+                                <p>2</p>
+                              </div>
+                              <div>
+                                <h2>ENGINES</h2>
+                                <p>2</p>
+                              </div>
+                             
+                              
+                    </div> 
+                     <img src="/steam.gif" alt="steam gif" />
+          </div>
+</section>
           </>
 )
 }

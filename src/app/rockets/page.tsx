f7e74fad363788a9a59b862b097f7bfe67645537
Dakitidunno <207196558+juanitoisback6@users.noreply.github.com>
2026-05-05 21:03:26 -0500

    Update page.tsx

--- src/app/rockets/page.tsx
+++ src/app/rockets/page.tsx
@@ -1,15 +1,12 @@
-
-
-
-
-
-
-
+'use client'
+ import AnimarAlScroll  from "@/Components/AnimateScroll";
+  
 export default function RocketsPage () {
+ 
 
 return(
           <>
-          <video 
+    <video 
     className="starshipLanding"
     autoPlay 
     muted 
@@ -22,17 +19,30 @@ return(
 </video>
 
 <img className="canvaArt" src="/Vector2.png" alt="" />
-
+<h2 className="rocketTitle">STARSHIP</h2>
 <section className="starcontainer">
-          <div className="rocketPartimg">
 
-                    <h2 className="rocketTitle">STARSHIP</h2>
-                    <img src="/starship.svg" alt="painter looking at the top of the page" />
+          <div className="rocketPartimg">
+            <AnimarAlScroll>
+              
+                    <img  
+                    src="/starship.svg" alt="STARSHIP IMAGE" />
+                    
+            </AnimarAlScroll>
           </div>
           <div className="rocketInfoPart">
-                    <h2>STARSHIP</h2>
-                    <p className="description">"With the ability to lift into orbit over 54 metric tons (119,000 lb)--a mass equivalent to a 737 jetliner loaded with passengers, crew, luggage and fuel--Falcon Heavy can lift more than twice the payload of the next closest operational vehicle, the Delta IV Heavy, at one-third the cost."</p>
+            
+            <AnimarAlScroll>
+                    <h2 className="rocketInfoPartTittle">STARSHIP</h2>
+            </AnimarAlScroll>
 
+            <AnimarAlScroll>
+              
+              <p className="description">"With the ability to lift into orbit over 54 metric tons (119,000 lb)--a mass equivalent to a 737 jetliner loaded with passengers, crew, luggage and fuel--Falcon Heavy can lift more than twice the payload of the next closest operational vehicle, the Delta IV Heavy, at one-third the cost."</p>
+
+            </AnimarAlScroll>
+                    
+<AnimarAlScroll> 
                     <div className="rocketS">
                               <div>
                                 <h2>STAGES</h2>
@@ -49,7 +59,7 @@ return(
                              
                               
                     </div> 
-                   
+ </AnimarAlScroll>                  
           </div>
 </section>
           </>
